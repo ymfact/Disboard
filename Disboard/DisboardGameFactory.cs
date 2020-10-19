cda63d84cc647cb88e1c79e8aaf7d6e05b3caf1c
ymfact <ymfact@gmail.com>
2020-10-19 17:53:44 +0900

    DisboardGameFactory to sealed & internal

--- Disboard/DisboardGameFactory.cs
+++ Disboard/DisboardGameFactory.cs
@@ -7,10 +7,12 @@ namespace Disboard
     /// Game은 파라미터가 GameInitializeData 1개인 public 생성자를 가져야 합니다.
     /// </summary>
     /// <typeparam name="GameType">파라미터가 GameInitializeData 1개인 public 생성자를 가진 Game입니다.</typeparam>
-    public class DisboardGameFactory<GameType> : IDisboardGameFactory where GameType : DisboardGame
+    public sealed class DisboardGameFactory<GameType> : IDisboardGameFactory where GameType : DisboardGame
     {
-        public DisboardGame New(DisboardGameInitData initData) => (DisboardGame)Activator.CreateInstance(typeof(GameType), new[] { initData })!;
+        internal DisboardGame New(DisboardGameInitData initData) => (DisboardGame)Activator.CreateInstance(typeof(GameType), new[] { initData })!;
+        DisboardGame IDisboardGameFactory.New(DisboardGameInitData initData) => New(initData);
 
-        public void OnHelp(DisboardChannel channel) => channel.Send("게임이 도움말을 제공하지 않습니다.");
+        internal void OnHelp(DisboardChannel channel) => channel.Send("게임이 도움말을 제공하지 않습니다.");
+        void IDisboardGameFactory.OnHelp(DisboardChannel channel) => OnHelp(channel);
     }
 }
