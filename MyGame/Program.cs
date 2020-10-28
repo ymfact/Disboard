e31d9e6471be4da67d2ebd930331181da96b0670
ymfact <ymfact@gmail.com>
2020-10-28 18:14:25 +0900

    Bot intent 설명 추가

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -13,8 +13,6 @@ namespace MyGame
 
 }
 
-/* https://discordpy.readthedocs.io/en/latest/discord.html
- * 위 페이지를 따라 토큰을 발급받은 뒤,
  * disboard.Run("Token.token");의 두 큰 따옴표를 제거하고,
  * 아래 내용과 같은 Token.cs를 생성하세요.
 
